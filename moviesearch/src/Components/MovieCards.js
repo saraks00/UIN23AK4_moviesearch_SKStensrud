c1e4fab4ba1dd6134bce0627d0c2266fd2a7f095
saraks00 <sarakristine@hotmail.no>
2023-03-28 23:42:57 +0200

    started styling

--- moviesearch/src/Components/MovieCards.js
+++ moviesearch/src/Components/MovieCards.js
@@ -5,8 +5,8 @@ export default function MovieCards({movies, setSearch}){
     return(
         <>
         <Search setSearch={setSearch} />
+        <h2>Filmer</h2>
         <section className="movie-list">
-            <h2>Filmer</h2>
             {movies.map((movie, index) => ( <MovieCard key={index} title={movie.Title} img={movie.Poster} year={movie.Year}
             gen={movie.Genre} dir={movie.Director} act={movie.Actors} awards={movie.Awards} slug={movie?.movie?.label.replace(/\s/g, "-").toLowerCase()}/>))}
         </section>
